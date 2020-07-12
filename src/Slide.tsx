21cf828a17174a0dc851fe2eba285d75a60fa6be
vscav <vscavinner@gmail.com>
2020-07-12 16:12:46 +0200

    Interpolate slides colors

--- src/Slide.tsx
+++ src/Slide.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import { Dimensions, StyleSheet, Text, View } from "react-native";
 
-const { width } = Dimensions.get("window");
+const { width, height } = Dimensions.get("window");
+
+export const SLIDE_HEIGHT = 0.61 * height;
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    width,
+  },
+  titleContainer: {
+    height: 100,
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 80,
+    lineHeight: 80,
+    fontFamily: "SFProText-Bold",
+    color: "#fff",
+    textAlign: "center",
+  },
 });
 
 interface SlideProps {
@@ -13,9 +28,16 @@ interface SlideProps {
 }
 
 const Slide = ({ label, right }: SlideProps) => {
+  const transform = [
+    { translateY: (SLIDE_HEIGHT - 100) / 2 },
+    { translateX: right ? width / 2 - 50 : -width / 2 + 50 },
+    { rotate: right ? "-90deg" : "90deg" },
+  ];
   return (
-    <View style={{ width }}>
-      <Text>{label}</Text>
+    <View style={styles.container}>
+      <View style={[styles.titleContainer, { transform }]}>
+        <Text style={styles.title}>{label}</Text>
+      </View>
     </View>
   );
 };
--- src/Slider.tsx
+++ src/Slider.tsx
@@ -1,41 +1,68 @@
 import React from "react";
 import { Dimensions, View, StyleSheet } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
+import { useValue, onScrollEvent, interpolateColor } from "react-native-redash";
 
-import Slide from "./Slide";
+import Slide, { SLIDE_HEIGHT } from "./Slide";
+import Animated from "react-native-reanimated";
 
 const { width, height } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#fff",
   },
   slider: {
-    height: 0.61 * height,
+    height: SLIDE_HEIGHT,
+    borderBottomRightRadius: 75,
   },
   footer: {
     flex: 1,
   },
 });
 
+const slides = [
+  { label: "#*!?", color: "#f89e4c" },
+  { label: "@&!*", color: "#f46e38" },
+  { label: "**!@", color: "#f04647" },
+  { label: "@$*!#", color: "#582841" },
+];
+
 const Slider = () => {
+  const x = useValue(0);
+  const onScroll = onScrollEvent({ x });
+  const backgroundColor = interpolateColor(x, {
+    inputRange: slides.map((_, i) => i * width),
+    outputRange: slides.map((slide) => slide.color),
+  });
   return (
     <View style={styles.container}>
-      <View style={styles.slider}>
-        <ScrollView
+      <Animated.View style={[styles.slider, { backgroundColor }]}>
+        <Animated.ScrollView
           horizontal
           snapToInterval={width}
           decelerationRate="fast"
           showsHorizontalScrollIndicator={false}
           bounces={false}
+          scrollEventThrottle={1}
+          {...{ onScroll }}
         >
-          <Slide label="#*!?" />
-          <Slide label="@&!*" right />
-          <Slide label="**!@" />
-          <Slide label="@$*!#" right />
-        </ScrollView>
+          {slides.map(({ label }, index) => (
+            <Slide key={index} right={!!(index % 2)} {...{ label }} />
+          ))}
+        </Animated.ScrollView>
+      </Animated.View>
+      <View style={styles.footer}>
+        <Animated.View
+          style={{
+            ...StyleSheet.absoluteFillObject,
+            backgroundColor,
+          }}
+        ></Animated.View>
+        <View
+          style={{ flex: 1, backgroundColor: "#fff", borderTopLeftRadius: 75 }}
+        ></View>
       </View>
-      <View style={styles.footer}></View>
     </View>
   );
 };
