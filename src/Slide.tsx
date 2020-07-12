c97e41593b24ee2218692d318249239cf689a75d
vscav <vscavinner@gmail.com>
2020-07-12 16:41:20 +0200

    Slides footer content

--- src/Slide.tsx
+++ src/Slide.tsx
@@ -23,11 +23,11 @@ const styles = StyleSheet.create({
 });
 
 interface SlideProps {
-  label: string;
+  title: string;
   right?: boolean;
 }
 
-const Slide = ({ label, right }: SlideProps) => {
+const Slide = ({ title, right }: SlideProps) => {
   const transform = [
     { translateY: (SLIDE_HEIGHT - 100) / 2 },
     { translateX: right ? width / 2 - 50 : -width / 2 + 50 },
@@ -36,7 +36,7 @@ const Slide = ({ label, right }: SlideProps) => {
   return (
     <View style={styles.container}>
       <View style={[styles.titleContainer, { transform }]}>
-        <Text style={styles.title}>{label}</Text>
+        <Text style={styles.title}>{title}</Text>
       </View>
     </View>
   );
--- src/Slider.tsx
+++ src/Slider.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Dimensions, View, StyleSheet } from "react-native";
 import { useValue, onScrollEvent, interpolateColor } from "react-native-redash";
+import Animated, { multiply } from "react-native-reanimated";
 
 import Slide, { SLIDE_HEIGHT } from "./Slide";
-import Animated from "react-native-reanimated";
+import Subslide from "./Subslide";
 
+const BORDER_RADIUS = 75;
 const { width, height } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
@@ -14,18 +16,48 @@ const styles = StyleSheet.create({
   },
   slider: {
     height: SLIDE_HEIGHT,
-    borderBottomRightRadius: 75,
+    borderBottomRightRadius: BORDER_RADIUS,
   },
   footer: {
     flex: 1,
   },
+  footerContent: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "#fff",
+    borderTopLeftRadius: BORDER_RADIUS,
+  },
 });
 
 const slides = [
-  { label: "#*!?", color: "#f89e4c" },
-  { label: "@&!*", color: "#f46e38" },
-  { label: "**!@", color: "#f04647" },
-  { label: "@$*!#", color: "#582841" },
+  {
+    title: "#*!?",
+    subtitle: "Terre battue dans les chaussettes",
+    description:
+      "Morbi tristique elementum cursus. Proin vitae neque bibendum, fringilla metus et, aliquam felis.",
+    color: "#f89e4c",
+  },
+  {
+    title: "@&!*",
+    subtitle: "Soleil dans les yeux",
+    description:
+      "Donec porttitor ligula id condimentum imperdiet. Nullam consequat molestie risus in rutrum.",
+    color: "#f46e38",
+  },
+  {
+    title: "**!@",
+    subtitle: "Spectateur bavard",
+    description:
+      "Proin ut turpis a orci ultricies dictum. Nunc posuere libero quis diam euismod, eu aliquam risus egestas.",
+    color: "#f04647",
+  },
+  {
+    title: "@$*!#",
+    subtitle: "Faux rebond",
+    description:
+      "Morbi sem erat, pretium vel fermentum quis, tincidunt sit amet neque.",
+    color: "#582841",
+  },
 ];
 
 const Slider = () => {
@@ -47,8 +79,8 @@ const Slider = () => {
           scrollEventThrottle={1}
           {...{ onScroll }}
         >
-          {slides.map(({ label }, index) => (
-            <Slide key={index} right={!!(index % 2)} {...{ label }} />
+          {slides.map(({ title }, index) => (
+            <Slide key={index} right={!!(index % 2)} {...{ title }} />
           ))}
         </Animated.ScrollView>
       </Animated.View>
@@ -58,10 +90,25 @@ const Slider = () => {
             ...StyleSheet.absoluteFillObject,
             backgroundColor,
           }}
-        ></Animated.View>
-        <View
-          style={{ flex: 1, backgroundColor: "#fff", borderTopLeftRadius: 75 }}
-        ></View>
+        />
+        <Animated.View
+          style={[
+            styles.footerContent,
+            {
+              width: width * slides.length,
+              flex: 1,
+              transform: [{ translateX: multiply(x, -1) }],
+            },
+          ]}
+        >
+          {slides.map(({ subtitle, description }, index) => (
+            <Subslide
+              key={index}
+              last={index === slides.length - 1}
+              {...{ subtitle, description }}
+            />
+          ))}
+        </Animated.View>
       </View>
     </View>
   );
