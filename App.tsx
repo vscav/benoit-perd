8d90a866edfb0147676f8cd83ecf1901f5b49f4f
vscav <vscavinner@gmail.com>
2020-07-14 12:17:55 +0200

    New logo (temporary)

--- App.tsx
+++ App.tsx
@@ -7,11 +7,6 @@ import Slider from "./src/Slider";
 import { LoadAssets, theme } from "./src/components";
 
 import { fonts } from "./src/store";
-// const fonts = {
-//   "SFProText-Bold": require("./assets/fonts/SF-Pro-Text-Bold.otf"),
-//   "SFProText-Semibold": require("./assets/fonts/SF-Pro-Text-Semibold.otf"),
-//   "SFProText-Regular": require("./assets/fonts/SF-Pro-Text-Regular.otf"),
-// };
 
 const styles = StyleSheet.create({
   container: {
