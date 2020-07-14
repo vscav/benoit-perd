4b0c3cb6f3baac7d3acdce1e0fb9baab98299c0a
vscav <vscavinner@gmail.com>
2020-07-14 10:48:20 +0200

    Create basic store

---
+++ src/store/modules/slides.ts
@@ -0,0 +1,38 @@
+export const slides = [
+  {
+    title: "#*!?",
+    subtitle: "Terre battue dans les chaussettes",
+    description:
+      "Morbi tristique elementum cursus. Proin vitae neque bibendum, fringilla metus.",
+    color: "#f89e4c",
+    picture: require("../../../assets/images/1.png"),
+    sound: require("../../../assets/sounds/1.wav"),
+  },
+  {
+    title: "@&!*",
+    subtitle: "Soleil dans les yeux",
+    description:
+      "Donec porttitor ligula id condimentum imperdiet. Nullam consequat molestie risus in rutrum.",
+    color: "#f46e38",
+    picture: require("../../../assets/images/2.png"),
+    sound: require("../../../assets/sounds/2.wav"),
+  },
+  {
+    title: "**!@",
+    subtitle: "Spectateur bavard",
+    description:
+      "Proin ut turpis a orci ultricies dictum. Nunc posuere libero quis diam euismod, eu aliquam risus egestas.",
+    color: "#f04647",
+    picture: require("../../../assets/images/3.png"),
+    sound: require("../../../assets/sounds/3.wav"),
+  },
+  {
+    title: "@$*!#",
+    subtitle: "Faux rebond",
+    description:
+      "Morbi sem erat, pretium vel fermentum quis, tincidunt sit amet neque.",
+    color: "#582841",
+    picture: require("../../../assets/images/4.png"),
+    sound: require("../../../assets/sounds/4.wav"),
+  },
+];
